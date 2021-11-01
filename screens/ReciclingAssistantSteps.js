11bf9efffd4e6838651f7ed320478fb9854536e5
Tobías Kleppe <tkleppe@uade.edu.ar>
2021-11-01 14:00:57 -0300

    Recicling Assistant component-conmutation logic

--- screens/ReciclingAssistantSteps.js
+++ screens/ReciclingAssistantSteps.js
@@ -1,22 +1,54 @@
 import {useState} from 'react';
+import ChoiceStepPage from '../components/steps/ChoiceStepPage';
+import DoubleStepPage from '../components/steps/DoubleStepPage';
+import ForkStepPage from '../components/steps/ForkStepPage';
+import NegativeStepPage from '../components/steps/NegativeStepPage';
+import SimpleStepPage from '../components/steps/SimpleStepPage';
+import FinalStepPage from '../components/steps/FinalStepPage';
+import TopBox from '../components/TopBox';
 
-const ReciclingAssistantSteps = () => {
-    // Dynamic Data:
-
-    // Name:
-    // Step , StepType => StepType tiene que seguir una convencion: 1 es simple, 2 es double, 3 es choice, 4 es fork (por ejemplo)
-    // Image or Images
-    const [currentStep, setCurrentStep] = useState(0)
-    const [currentData, setCurrentData] = useState({});
+const ReciclingAssistantSteps = ({steps}) => {
+    const [currentStep, setCurrentStep] = useState(steps[0])
     const [negativeStep, setNegativeStep] = useState(false);
+    const [finalStep, setFinalStep] = useState(false);
     
     const handleNextStep = () => {
-        setCurrentStep(currentStep+1);
+        if(currentStep.stepNumber==steps.length){
+            setFinalStep(true);
+        }
+        else{
+            setCurrentStep(steps[currentStep+1]);
+        }
+    }
+
+    const handleReturnToPrevious = () => {
+        setNegativeStep(false);
+    }
+
+    const displayStep = (stepType) => {
+        switch(stepType){
+            case 1:
+                return <SimpleStepPage step={currentStep} handleNextStep={()=>handleNextStep()} handleNegativeStep={() => setNegativeStep(true)}/>
+            case 2:
+                return <DoubleStepPage step={currentStep} handleNextStep={()=>handleNextStep()} handleNegativeStep={() => setNegativeStep(true)}/>
+            case 3:
+                return <ChoiceStepPage step={currentStep} handleNextStep={()=>handleNextStep()} handleNegativeStep={() => setNegativeStep(true)}/>
+            case 4:
+                return <ForkStepPage step={currentStep} handleNextStep={()=>handleNextStep()} handleNegativeStep={() => setNegativeStep(true)}/>
+        }
     }
 
     return(
-        <>
-        </>
+        <Center>
+            <TopBox />
+            {!finalStep ?
+                (!negativeStep ?
+                    displayStep(currentStep.stepType) :
+                    <NegativeStepPage stepName={currentStep.stepName} handleReturnToPrevious={() => handleReturnToPrevious()}/>
+                ) : 
+                <FinalStepPage />
+            }
+        </Center>
     );
 }
 
