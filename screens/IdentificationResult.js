2142be58a40950489a783737c918268f6bb4c868
leoangel1994 <52744105+leoangel1994@users.noreply.github.com>
2021-11-02 18:10:36 -0300

    Revert "Pilas added"
    This reverts commit b08a48bbef899afa9a9d807c852f88f9e812151c.

--- screens/IdentificationResult.js
+++ screens/IdentificationResult.js
@@ -3,7 +3,6 @@ import plasticSteps from './../assets/material-recicling-steps/plastico.json';
 import metalSteps from './../assets/material-recicling-steps/metal.json';
 import paperSteps from './../assets/material-recicling-steps/paper.json';
 import glassSteps from './../assets/material-recicling-steps/vidrio.json';
-import pilasSteps from './../assets/material-recicling-steps/pilas.json';
 import cartonSteps from './../assets/material-recicling-steps/carton.json';
 import ReciclingAssistantSteps from './ReciclingAssistantSteps';
 
@@ -17,8 +16,6 @@ const IdentificationResult = () => {
                 return <ReciclingAssistantSteps steps={plasticSteps} />
             case "VIDRIO":
                 return <ReciclingAssistantSteps steps={glassSteps} />
-            case "Pilas":
-                return <ReciclingAssistantSteps steps={pilasSteps} />
             case "PAPEL":
                 return <ReciclingAssistantSteps steps={paperSteps} />
             case "CARTON":
