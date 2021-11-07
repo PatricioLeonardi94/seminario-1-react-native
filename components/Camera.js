fded5b738687af881d23d61c1ab702d59a30d1c4
Tobías Kleppe <tkleppe@uade.edu.ar>
2021-11-07 18:21:20 -0300

    WIP: camera component added

---
+++ components/Camera.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect, useRef } from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { Camera } from "expo-camera";
+import { MaterialIcons } from "@expo/vector-icons";
+
+export default function CameraScreen() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  const cam = useRef();
+
+  const _takePicture = async () => {
+    if (cam.current) {
+      const option = { quality: 0.5, base64: true, skipProcessing: false };
+      let photo = await cam.current.takePictureAsync(option);
+
+      const source = photo.uri;
+
+      if (source) {
+        cam.current.resumePreview();
+
+        console.log("picture source", source);
+      }
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type} ref={cam}>
+        <View style={styles.buttonContainer}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "flex-end",
+            }}
+          >
+            <View>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => _takePicture()}
+              >
+                <Text style={styles.text}> Take Photo </Text>
+                <MaterialIcons name="camera" size={50} color="white" />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Camera>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
+  },
+});
