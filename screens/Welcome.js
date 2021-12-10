adc8945a64e3c3394d5bd4bbe91ae4f0e95ddd1f
PatricioLeonardi94 <patricioleonardi@gmail.com>
2021-12-09 23:32:45 -0300

    Added new welcome page
    with the logos and everything

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,5 +1,5 @@
 import TopBox from "../components/TopBox";
-import { Box, Center, Image, Text, VStack } from "native-base";
+import { Box, Center, Image, Text, VStack, HStack } from "native-base";
 import React, { useState, useContext } from "react";
 
 //credentialContext
@@ -14,64 +14,150 @@ const Welcome = ({ navigation }) => {
   return (
     <Box>
       <TopBox navigation={navigation} />
-      <VStack alignItems="center">
-        <Center>
-          <Text fontFamily="body" fontSize={24} color="rgba(0, 0, 0, 0.4)">
-            {player.user.fullname}
-          </Text>
-          <Text
-            ml={"13.89%"}
-            mr={"13.89%"}
-            mt={"20.94%"}
-            mb={"65.62%"}
-            fontFamily="body"
-            fontWeight={500}
-            fontSize={24}
-            lineHeight={24}
-            display="flex"
-            alignItems="center"
-            textAlign="center"
-            letterSpacing={0.15}
-            color="rgba(0, 0, 0, 0.4)"
-          >
-            ¡Bienvenido a tu asistente de reciclado!
-          </Text>
-        </Center>
-        <Center
-          pos="absolute"
-          height={90}
-          width={180}
-          ml={"0%"}
-          mt={"-30%"}
-          borderRadius={"8px"}
-          bgColor="#84D31E"
+      <Center>
+        <Text
+          fontFamily="body"
+          fontWeight={500}
+          fontSize={24}
+          lineHeight={24}
+          display="flex"
+          letterSpacing={0.15}
+          opacity="0.4"
+          textAlign="center"
+          alignItems="center"
         >
-          <Text
-            pos="absolute"
-            color="white"
-            font="body"
-            fontWeight={500}
-            fontSize={26}
-            lineHeight={30}
-            display="flex"
-            alignItems="center"
-            textAlign="center"
-            letterSpacing={0.16}
-            onPress={() => navigation.navigate("Instrucciones")}
-          >
-            EMPEZAR
-          </Text>
-        </Center>
-        <Center>
-          <Image
-            w={366.11}
-            h={349}
-            opacity={0.8}
-            source={require("../assets/images/bottom-bubbles.png")}
-            alt=""
-          />
-        </Center>
-      </VStack>
+          Bienvenido a tu asistente de reciclado
+        </Text>
+      </Center>
+      <Box p="10">
+        <VStack space={10} alignItems="center">
+          <HStack space={10} alignItems="center">
+            <Center
+              backgroundColor="rgba(132, 211, 30, 0.2)"
+              width="150"
+              height="180"
+              alignItems="center"
+              borderWidth="1px"
+              borderColor="#84D31E"
+              borderRadius="10px"
+            >
+              <Image
+                top="0.5"
+                w="32"
+                h="32"
+                source={require("../assets/images/plastic_bin.png")}
+                alt="plastic bin"
+              />
+
+              <Text
+                top="3"
+                color="#84D31E"
+                fontFamily="body"
+                fontWeight={500}
+                fontSize={26}
+                lineHeight={24}
+                display="flex"
+                alignItems="center"
+                letterSpacing={0.15}
+                textAlign="center"
+              >
+                RECICLAR
+              </Text>
+            </Center>
+            <Center
+              width="150"
+              height="180"
+              borderWidth="1px"
+              borderColor="#84D31E"
+              borderRadius="10px"
+            >
+              <Image
+                top="0.5"
+                w="32"
+                h="32"
+                source={require("../assets/images/scores.png")}
+                alt="scores"
+              />
+              <Text
+                top="3"
+                color="#84D31E"
+                fontFamily="body"
+                fontWeight={500}
+                fontSize={26}
+                lineHeight={24}
+                display="flex"
+                alignItems="center"
+                letterSpacing={0.15}
+                textAlign="center"
+              >
+                RANKING
+              </Text>
+            </Center>
+          </HStack>
+          <HStack space={10} alignItems="center">
+            <Center
+              borderColor="#84D31E"
+              width="150"
+              height="180"
+              borderWidth="1px"
+              borderColor="#84D31E"
+              borderRadius="10px"
+            >
+              <Image
+                top="0.5"
+                w="32"
+                h="32"
+                source={require("../assets/images/medals.png")}
+                alt="medals"
+              />
+              <Text
+                top="3"
+                color="#84D31E"
+                fontFamily="body"
+                fontWeight={500}
+                fontSize={26}
+                lineHeight={24}
+                display="flex"
+                alignItems="center"
+                letterSpacing={0.15}
+                textAlign="center"
+              >
+                MEDALLAS
+              </Text>
+            </Center>
+            <Box backgroundColor="rgba(132, 211, 30, 0.2)" borderRadius="10px">
+              <Center
+                width="150"
+                height="180"
+                borderWidth="1px"
+                borderColor="#84D31E"
+                borderRadius="10px"
+              >
+                <Image
+                  top="0.5"
+                  w="32"
+                  h="32"
+                  source={require("../assets/images/stars.png")}
+                  alt="stars"
+                />
+                <Text
+                  top="3"
+                  color="#84D31E"
+                  fontFamily="body"
+                  fontWeight={500}
+                  fontSize={26}
+                  lineHeight={24}
+                  display="flex"
+                  alignItems="center"
+                  letterSpacing={0.15}
+                >
+                  CANJEAR
+                </Text>
+              </Center>
+            </Box>
+          </HStack>
+        </VStack>
+      </Box>
     </Box>
   );
 };
